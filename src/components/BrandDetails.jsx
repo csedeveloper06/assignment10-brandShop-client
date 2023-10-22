96f8ae927ab887b5988a03d66384c4580e983017
farida farida <faridayasmeencse@gmail.com>
2023-10-22 13:13:22 +0600

    enhanced up the cards section in the brand details page

--- src/components/BrandDetails.jsx
+++ src/components/BrandDetails.jsx
@@ -21,7 +21,7 @@ const BrandDetails = ({brandProduct}) => {
            
 
             {/* Featured Products */}
-            <div>
+            <div className="grid grid-cols-1 ml-16 md:grid-cols-2 md:ml-0 lg:ml-0 lg:grid-cols-3 my-8 gap-4">
                 {
                     brandProducts.map(brandProduct => <Product
                         key={brandProduct._id}
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,18 +5,20 @@ const Product = ({brandProduct}) => {
     const {photo,name,brand,type,price,rating,description} = brandProduct
 
     return (
-        <div className="card card-side bg-base-100 shadow-xl">
-            <figure><img src={photo} alt="product"/></figure>
+        <div className="card w-80 bg-base-100 shadow-xl">
+            <figure><img className="w-[100%] h-40" src={photo} alt="product"/></figure>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <span>{brand}</span>
-                <span>{type}</span>
-                <span>{price}</span>
-                <p>{description}</p>
-                <img src={rating} alt="" />
-                <div className="card-actions justify-center">
-                    <button className="btn btn-warning">Details</button>
-                    <button className="btn btn-success">Update</button>
+                <h2 className="card-title">Title: {name}</h2>
+                <div className="flex">
+                    <p className="text-xs">Brand:{brand}</p>
+                    <p className="text-xs">Product Type:{type}</p>  
+                </div>
+                <p className="text-xs">Price:{price}</p>
+                <p className="text-xs">Description: {description}</p>
+                <img className="w-44 h-8" src={rating} alt="" />
+                <div className="card-actions justify-start">
+                    <button className="btn btn-warning btn-sm">Details</button>
+                    <button className="btn btn-success btn-sm">Update</button>
                 </div>
             </div>
         </div>
